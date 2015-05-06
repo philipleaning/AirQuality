93c57d4d198f3c5bf7542fb90c79ab9355291090
philipleaning <philipleaning@gmx.com>
2015-05-06 13:55:11 +0100

    Tabbed View App Template. Standard Apple template

---
+++ AirQualityDemo/AirQualityDemo/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  AirQualityDemo
+//
+//  Created by Philip Leaning on 06/05/2015.
+//  Copyright (c) 2015 CCMOS Ltd. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
